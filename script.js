a68dcc72a6215ab7be507b921b45fd0064b95bbd
Leticia Harumi <80927546+leticiaharumi@users.noreply.github.com>
2023-08-24 22:16:01 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var hamburger = document.getElementById('hamburger');
+var itensMenu = document.getElementById('itens_menu');
+
+hamburger.addEventListener('click', aparecer);
+
+function aparecer(){
+    if(itensMenu.style.display == 'block'){
+        itensMenu.classList.remove('show');
+    }else{
+        itensMenu.classList.toggle('show');
+    }
+}
